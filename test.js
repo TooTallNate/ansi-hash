77467d4c142c81034acd04d2961a236e504ec781
Nathan Rajlich <nathan@tootallnate.net>
2018-02-27 17:23:26 -0800

    Log the alphabet

--- test.js
+++ test.js
@@ -6,9 +6,20 @@ function log(v) {
   console.log(chalk.ansi256(color)(v), color);
 }
 
+for (let i = 'a'; i < 'z'; i = String.fromCharCode(i.charCodeAt(0) + 1)) {
+  log(i);
+}
+log('test');
 log('now-sync-c32bfb0f37036ba54d1b691f1775b1aa-r7hs6');
 log('now-sync-c32bfb0f37036ba54d1b691f1775b1aa-ncf3r');
 log('now-sync-c32bfb0f37036ba54d1b691f1775b1aa-w0dgt');
 log('now-sync-c32bfb0f37036ba54d1b691f1775b1aa-2nfz5');
 log('now-sync-c32bfb0f37036ba54d1b691f1775b1aa-3l5z0');
 log('now-sync-c32bfb0f37036ba54d1b691f1775b1aa-7c2rg');
+
+/*
+// print out available colors
+for (const color of hash.colors) {
+  console.log(chalk.ansi256(color)(String(color)), color);
+}
+*/
